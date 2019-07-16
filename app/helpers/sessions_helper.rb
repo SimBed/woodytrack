69731b6ed9d43461ba8402daad15087555f4926c
SimBed <dansimbed@gmail.com>
2019-07-16 19:57:55 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,39 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
-  # Returns the current logged-in user (if any).
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # Returns the user corresponding to the remember token cookie.
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
+  
+  #more understandable code in (without the 'assignment')
+  #if session[:user_id]
+  #@current_user ||= User.find_by(id: session[:user_id])
+#elsif cookies.signed[:user_id]
+ #user = User.find_by(id: cookies.signed[:user_id])
+  #if user && user.authenticated?(cookies[:remember_token])
+    #log_in user
+   # @current_user = user
+  #end
+#end
+  
+      #@current_user ||= User.find_by(id: session[:user_id])
       # complicated version of
       
       #@current_user = @current_user || User.find_by(id: session[:user_id])
@@ -19,16 +48,23 @@ module SessionsHelper
       #else
       #  @current_user
       #end
-    end
-  end
+
   
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
   
- # Logs out the current user.
+ # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
