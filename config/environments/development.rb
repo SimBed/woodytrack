8693674840c94925aad87848acf5ad9d86b78b28
SimBed <dansimbed@gmail.com>
2019-08-31 22:08:17 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'https://93ce19a2e84b4cdb884784408259a84e.vfs.cloud9.eu-west-1.amazonaws.com/' 
+  # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
