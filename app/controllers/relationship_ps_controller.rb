99e827b0132ddaf909059786ceb7dee463488803
SimBed <dansimbed@gmail.com>
2019-11-07 19:40:00 +0000

    Following problems working

--- app/controllers/relationship_ps_controller.rb
+++ app/controllers/relationship_ps_controller.rb
@@ -1,7 +1,7 @@
 class RelationshipPsController < ApplicationController
   before_action :set_rel, only: [:edit, :update, :destroy]
   ##  before_action :admin_user,     only: [:new, :edit, :update, :destroy]
-    
+
   def index
     @problems = current_user.problems
   end
@@ -16,14 +16,23 @@ class RelationshipPsController < ApplicationController
   # POST /problems
   # POST /problems.json
   def create
-    @relationship_ps = RelationshipP.new(rel_params)
-
-      if @relationship_ps.save
+    if params[:commit] == 'Follow'
+         @problem = Problem.find(params[:problem_id])
+    current_user.probfollow(@problem)
+    flash[:success] = "Problem followed!"
+            redirect_to problems_path
+    else
+   @relationship_ps = RelationshipP.new(rel_params)
+       if @relationship_ps.save
         redirect_to relationship_ps_path
-        flash[:success] = "New problem added!"
-      else
+        flash[:success] = "New problem followed! Edit from My Problems."
+       else
        render :new 
-      end
+       end
+    end
+ 
+
+  
   end
   
   def update
@@ -36,8 +45,11 @@ class RelationshipPsController < ApplicationController
   end
   
   def destroy
+    #@user = Relationship.find(params[:id]).followed
+    #current_user.unfollow(@user)
+    
     @relationship_ps.destroy
-    flash[:success] = "Problem deleted"
+    flash[:success] = "Problem no longer followed"
     redirect_to relationship_ps_path
   end
 
@@ -49,7 +61,8 @@ class RelationshipPsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rel_params
-      params.require(:relationship_p).permit(:suggestedgrade, :highpoint, :dohp, :firsttry, :rating)
+      #params.require(:relationship_p).permit(:suggestedgrade, :highpoint, :dohp, :firsttry, :rating)
+       params.require(:relationship_p).permit(:user_id, :problem_id, :suggestedgrade, :highpoint, :dohp, :firsttry, :rating)
     end
-
+    
 end
