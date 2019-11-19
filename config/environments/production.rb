f441856a0f6baa11b6a99a8751a7ff3be1055d11
SimBed <dansimbed@gmail.com>
2019-11-19 13:00:24 +0000

    Heroku back on track

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nameless-gorge-10684.herokuapp.com'
+  host = 'afternoon-reaches-09920.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
